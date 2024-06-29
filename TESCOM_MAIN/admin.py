b3f39390b278fbf5ab75e6a9333ba25668c26f73
MistersGentleman33 <88989163+MistersGentleman33@users.noreply.github.com>
2024-06-28 19:12:30 -0600

    Modificacion de la declaracion de los modelos
    No afecta ni mejora el funcionamiento sino es para tener un poco de orden

--- TESCOM_MAIN/admin.py
+++ TESCOM_MAIN/admin.py
@@ -3,9 +3,33 @@ from TESCOM_MAIN.models import *
 
 # Register your models here.
 
-admin.site.register(Alumno)
-admin.site.register(TrabajoTerminal)
-admin.site.register(Profesor)
-admin.site.register(AlumnoTrabajoTerminal)
-admin.site.register(HorarioDisponible)
-admin.site.register(Observaciones)
+@admin.register(HorarioDisponible)
+class HorarioDisponibleAdmin(admin.ModelAdmin):
+    # list_display = ('profesor', 'dia_semana', 'horario_inicio', 'horario_fin')
+    #search_fields = ('profesor',)
+    pass
+
+
+@admin.register(Profesor)
+class ProfesorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TrabajoTerminal)
+class TrabajoTerminalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AlumnoTrabajoTerminal)
+class AlumnoTrabajoTerminalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Observaciones)
+class ObservacionesAdmin(admin.ModelAdmin):
+    pass
